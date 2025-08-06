8a2ada3ce4513a79cc9d798ef5ac4dc144807bc7
Haochen Wu <wuhaochen42@gmail.com>
2025-08-06 14:44:20 +0800

    optimize bundle

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-})
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    visualizer({
+      open: true, // opens report in browser
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("ant")) {
+              return "vendor-antd";
+            }
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
