af7293f0fa5a9ab89cdfab7174ee43ca440f49ab
Haochen Wu <wuhaochen42@gmail.com>
2025-08-18 12:56:40 +0800

    minor refactor

--- internal/auth/oidc.go
+++ internal/auth/oidc.go
@@ -4,12 +4,16 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lestrrat-go/jwx/v3/jwk"
 	"github.com/lestrrat-go/jwx/v3/jwt"
 	"golang.org/x/oauth2"
 )
+
+const LOGIN_PATH = "/login"
+const CALLBACK_PATH = "/auth/callback"
 
 func randB64URL(n int) string {
 	b := make([]byte, n)
@@ -28,10 +32,10 @@ type OIDCConfig struct {
 
 func (c *OIDCConfig) RegisterTo(router *gin.Engine) {
 	router.Use(c.Authorize)
-	GrantPublicAccess("/login")
-	router.GET("/login", c.Login)
-	GrantPublicAccess("/auth/callback")
-	router.GET("/auth/callback", c.Callback)
+	GrantPublicAccess(LOGIN_PATH)
+	router.GET(LOGIN_PATH, c.Login)
+	GrantPublicAccess(CALLBACK_PATH)
+	router.GET(CALLBACK_PATH, c.Callback)
 }
 
 func (c *OIDCConfig) Authorize(ctx *gin.Context) {
@@ -70,7 +74,7 @@ func (c *OIDCConfig) Login(ctx *gin.Context) {
 	http.SetCookie(ctx.Writer, &http.Cookie{
 		Name:     "after_login",
 		Value:    afterLogin,
-		Path:     "/",
+		Path:     CALLBACK_PATH,
 		HttpOnly: true,
 		Secure:   true,
 	})
@@ -84,13 +88,28 @@ func (c *OIDCConfig) Callback(ctx *gin.Context) {
 	cState, _ := ctx.Cookie("oidc_state")
 	if state != cState {
 		ctx.JSON(http.StatusBadRequest, nil)
+		return
 	}
 	afterLogin, _ := ctx.Cookie("after_login")
-	token, _ := c.OAuthConfig.Exchange(ctx, ctx.Query("code"))
+	token, err := c.OAuthConfig.Exchange(ctx, ctx.Query("code"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, nil)
+		return
+	}
 	http.SetCookie(ctx.Writer, &http.Cookie{
 		Name:     "id_token",
 		Value:    token.Extra("id_token").(string),
 		Path:     "/",
+		HttpOnly: true,
+		Secure:   true,
+	})
+	// Remove redirect cookie
+	http.SetCookie(ctx.Writer, &http.Cookie{
+		Name:     "after_login",
+		Value:    "",
+		Path:     CALLBACK_PATH,
+		MaxAge:   -1,
+		Expires:  time.Unix(0, 0),
 		HttpOnly: true,
 		Secure:   true,
 	})
